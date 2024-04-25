15b344d9bb39e939d6811af8f7a20f19f88312e3
Servyn <cuongthientai13@gmail.com>
2024-04-25 18:19:51 +0700

    chức năng sửa

---
+++ WebTinTuc/Models/EditArticleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace WebTinTuc.Models
+{
+    public class EditArticleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter content")]
+        [AllowHtml]
+        public string Content { get; set; }
+
+        [Required(ErrorMessage = "Please enter a publish date")]
+        public DateTime PublishDate { get; set; }
+    }
+}
